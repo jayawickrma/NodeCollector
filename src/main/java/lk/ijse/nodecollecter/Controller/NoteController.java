3d6734dff6728651cdbc24e0de3c3edb0aa3c50a
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-09-07 15:44:37 +0530

    id generator

--- src/main/java/lk/ijse/nodecollecter/Controller/NoteController.java
+++ src/main/java/lk/ijse/nodecollecter/Controller/NoteController.java
@@ -1,9 +1,32 @@
 package lk.ijse.nodecollecter.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.ijse.nodecollecter.DTO.IMPL.NoteDTO;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/notes")
 public class NoteController {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String saveNote(@RequestBody NoteDTO noteDTO){
+        return "Note Saved Successfully";
+    }
+    @GetMapping
+    public String getSelectedNote(){
+        return null;
+    }
+    @GetMapping
+    public List<NoteDTO>  getAllNotes(){
+        return null;
+    }
+    @DeleteMapping
+    public void deleteNote(String noteID){
+
+    }
+    @PutMapping
+    public void updateNote(String noteID,NoteDTO noteDTO){
+
+    }
 }
---
+++ src/main/java/lk/ijse/nodecollecter/Utill/AppUtill.java
@@ -0,0 +1,9 @@
+package lk.ijse.nodecollecter.Utill;
+
+import java.util.UUID;
+
+public class AppUtill {
+    public static String generateNoteID(){
+        return "NOTE -" +  UUID.randomUUID();
+    }
+}
