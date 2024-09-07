f47e408e5b3e8dba0a41cd49860808007a526cf4
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-09-07 13:20:31 +0530

    created Dtos,controllers

---
+++ src/main/java/lk/ijse/nodecollecter/DTO/IMPL/UserDTO.java
@@ -0,0 +1,19 @@
+package lk.ijse.nodecollecter.DTO.IMPL;
+
+import lk.ijse.nodecollecter.DTO.SuperDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserDTO implements SuperDTO {
+    public String id;
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String password;
+    public String profilePic;
+    public String notes;
+}
